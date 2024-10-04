82f774bbcbb501b261fab42624e45bc666b1f4c8
msquaredds <alex.melesko@msquaredds.com>
2024-10-04 18:51:22 -0400

    Adding the individual regressions for box score

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -1,3 +1,6 @@
+COL_NAME_REPLACEMENTS = {
+    "+/-": "PLUS_MINUS"
+}
 INDEPENDENT_BOX_COLS = [
     'PTS', 'FGA', 'FG%', '3PA', '3P%', 'FTA', 'FT%', 'OREB', 'DREB', 'REB',
     'AST', 'TOV', 'STL', 'BLK', 'PF'
--- NBAPlayerValue.py
+++ NBAPlayerValue.py
@@ -1,3 +1,5 @@
+from selectors import SelectSelector
+
 import pandas as pd
 import streamlit as st
 
@@ -84,6 +86,14 @@ def main():
     ######################################################################
     team_box_score_data = pd.read_excel("NBAPlayerValueData.xlsx",
                                         "TeamBoxScores")
+    # rename columns to replace % with _P and / with _ and any other
+    # specifics
+    team_box_score_data.columns = [col.replace("%", "_P").replace("/", "_")
+                                   for col in team_box_score_data.columns]
+    team_box_score_data.columns = [
+        col if col not in gv.COL_NAME_REPLACEMENTS.keys()
+        else gv.COL_NAME_REPLACEMENTS[col]
+        for col in team_box_score_data.columns]
     with game_value_cols[1]:
         with st.expander("Team Box Score Data"):
             st.dataframe(team_box_score_data)
