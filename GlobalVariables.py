26d536017b96209207beab0cd64ea1b3000d26c1
msquaredds <alex.melesko@msquaredds.com>
2024-10-07 09:00:21 -0400

    Formatting the individual regression results

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -2,7 +2,7 @@ COL_NAME_REPLACEMENTS = {
     "+/-": "PLUS_MINUS"
 }
 INDEPENDENT_BOX_COLS = [
-    'PTS', 'FGA', 'FG_P', '3PA', '3P_P', 'FTA', 'FT_P', 'OREB', 'DREB',
+    'PTS', 'FGA', 'FG%', '3PA', '3P%', 'FTA', 'FT%', 'OREB', 'DREB',
     'REB', 'AST', 'TOV', 'STL', 'BLK', 'PF'
 ]
 DEPENDENT_BOX_COL = 'PLUS_MINUS'
