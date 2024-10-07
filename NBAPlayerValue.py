a6a893a7db93e652c56215cd8d4beab1ce51dcba
msquaredds <alex.melesko@msquaredds.com>
2024-10-07 09:31:10 -0400

    Formatting the individual regression results

--- NBAPlayerValue.py
+++ NBAPlayerValue.py
@@ -123,13 +123,13 @@ def main():
 
     results_df = pd.DataFrame(results).T
 
-    individual_stats_header_cols = st.columns([.05, .20, .25, .25, .25])
+    individual_stats_header_cols = st.columns([.05, .25, .25, .25, .25])
     with individual_stats_header_cols[1]:
         st.markdown("#### Individual Stat Analysis")
     with individual_stats_header_cols[2]:
         st.markdown("#### Sensitivity")
 
-    individual_stats_cols = st.columns([.05, .20, .25, .25, .25])
+    individual_stats_cols = st.columns([.05, .25, .25, .25, .25])
     with individual_stats_cols[1]:
         st.dataframe(
             results_df,
